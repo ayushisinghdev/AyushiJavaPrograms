0452e99e06088da01757f531177f08fcbc68a8c1
ayushisinghdev <ayushidma@gmail.com>
2022-02-11 01:52:20 +0530

    Selenium files

---
+++ Automation framework/src/configFiles/Selenium1.java
@@ -0,0 +1,24 @@
+package configFiles;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Selenium1 {
+
+	public static void main(String[] args) {
+		//set the path of chromedriver
+		System.setProperty("webdriver.chrome.driver", "/home/ayushi/Downloads/chromedriver_linux64/chromedriver");  
+		//Starting browser
+		WebDriver driver=new ChromeDriver(); 
+		//enter url
+		driver.get("https://arintra.com/");
+		String title = driver.getTitle(); 
+		System.out.println(title);
+		//Verify if title is correct
+		if(title.equals("Automated Clinical Documentation | Autonomous Coding")){
+			System.out.println("Correct title");
+		}
+		else{
+			System.out.println("Wrong title");
+		}
+	}
+
+}
