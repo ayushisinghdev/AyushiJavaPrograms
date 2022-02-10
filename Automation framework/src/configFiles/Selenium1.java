12464355008b1969a9862b396c96fde7bed353d0
ayushisinghdev <ayushidma@gmail.com>
2022-02-11 02:00:44 +0530

    Selenium

--- Automation framework/src/configFiles/Selenium1.java
+++ Automation framework/src/configFiles/Selenium1.java
@@ -1,22 +1,25 @@
 package configFiles;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Selenium1 {
 
 	public static void main(String[] args) {
-		//set the path of chromedriver
-		System.setProperty("webdriver.chrome.driver", "/home/ayushi/Downloads/chromedriver_linux64/chromedriver");  
-		//Starting browser
-		WebDriver driver=new ChromeDriver(); 
-		//enter url
+		// set the path of chromedriver
+		System.setProperty("webdriver.chrome.driver",
+				"/home/ayushi/Downloads/chromedriver_linux64/chromedriver");
+		// Starting browser
+		WebDriver driver = new ChromeDriver();
+		// enter url
 		driver.get("https://arintra.com/");
-		String title = driver.getTitle(); 
+		String title = driver.getTitle();
 		System.out.println(title);
-		//Verify if title is correct
-		if(title.equals("Automated Clinical Documentation | Autonomous Coding")){
+		// Verify if title is correct
+		if (title
+				.equals("Automated Clinical Documentation | Autonomous Coding")) {
 			System.out.println("Correct title");
-		}
-		else{
+		} else {
 			System.out.println("Wrong title");
 		}
 	}
